8b4f3045d5f49be334719ea77d19e23825bd6606
Ivan Murashov <murashov.alef@inbox.ru>
2023-01-29 23:12:18 +0300

    Добавил к циклу задачу с процентами...

--- learn_js/cycle_learn.js
+++ learn_js/cycle_learn.js
@@ -13,4 +13,14 @@ function lol() {
     return res.join(' ');
 }
 
+// функция, которая возвращает процент числа value от значения total
+
 console.log(lol())
+
+let value = 4;
+let total = 80;
+function getPercent(value, total) {
+    let res = value * 100 / total;
+    return res;
+}
+console.log(getPercent(value, total));
